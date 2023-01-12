97da1e56b3332eada04c82242f6a9c5c977297a2
HowieLi <chinahowieli@gmail.com>
2023-01-13 01:02:51 +0800

    update.

--- bilibili.js
+++ bilibili.js
@@ -6,8 +6,8 @@ https://*.(bilibili\.com|biliapi\.net)
 const feed_path = "/x/v2/feed/index?access_key"
 
 let url = $request.url
-let body = JSON.parse($response.body);
 console.log(url)
+let body = JSON.parse($response.body);
 if (url.indexOf(feed_path) != -1) {
     body = {
         "code": 0,

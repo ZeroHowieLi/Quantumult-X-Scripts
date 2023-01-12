5def830406961bf9dbb006baa873880ab4e8bba0
HowieLi <chinahowieli@gmail.com>
2023-01-12 23:51:56 +0800

    update.

--- bilibili.js
+++ bilibili.js
@@ -1,9 +1,12 @@
+// ^https:\/\/(app|data)\.(bilibili|biliapi)\.(com|net)\/?.?
+
+var url = $response.url;
 var body = $response.body;
 var obj = JSON.parse(body);
 
 obj['result'] = 0;
-body = JSON.stringify(obj);
+// body = JSON.stringify(obj);
 
-console.log(body);
+console.log(url);
 
 $done(body);

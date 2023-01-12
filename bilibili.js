b99f35934d34dfe6e1f7ce4317144b90bb38b912
HowieLi <chinahowieli@gmail.com>
2023-01-13 02:00:44 +0800

    update.

--- bilibili.js
+++ bilibili.js
@@ -29,61 +29,8 @@ if (url.indexOf(feed_path) != -1) {
           "search_entrance" : 5
         },
         "data" : {
-          "tab" : [
-            {
-              "id" : 39,
-              "tab_id" : "直播tab",
-              "name" : "好好",
-              "uri" : "bilibili://user_center/favourite",
-              "pos" : 1
-            },
-            {
-              "id" : 40,
-              "tab_id" : "推荐tab",
-              "default_selected" : 1,
-              "name" : "学习",
-              "uri" : "bilibili://user_center/favourite",
-              "pos" : 2
-            },
-            {
-              "id" : 41,
-              "tab_id" : "hottopic",
-              "name" : "24",
-              "uri" : "bilibili://user_center/favourite",
-              "pos" : 3
-            },
-            {
-              "id" : 545,
-              "tab_id" : "bangumi",
-              "name" : "24",
-              "uri" : "bilibili://user_center/favourite",
-              "pos" : 4
-            },
-            {
-              "id" : 151,
-              "tab_id" : "film",
-              "name" : "成功",
-              "uri" : "bilibili://user_center/favourite",
-              "pos" : 5
-            },
-            {
-              "pos" : 6,
-              "id" : 136117,
-              "tab_id" : "165",
-              "name" : "上岸",
-              "uri" : "bilibili://user_center/favourite",
-              "color" : "#DD1225"
-            }
-          ],
+          "tab" : [],
           "top" : [
-            {
-              "id" : 222,
-              "icon" : "http://i0.hdslb.com/bfs/archive/734a3b610a953df398bbe6d787944514dcd94a46.png",
-              "tab_id" : "游戏中心Top",
-              "name" : "游戏中心",
-              "uri" : "bilibili://game_center/home",
-              "pos" : 1
-            },
             {
               "id" : 176,
               "icon" : "http://i0.hdslb.com/bfs/archive/d43047538e72c9ed8fd8e4e34415fbe3a4f632cb.png",
@@ -94,13 +41,6 @@ if (url.indexOf(feed_path) != -1) {
             }
           ],
           "top_more" : [
-            {
-              "id" : 621,
-              "icon" : "http://i0.hdslb.com/bfs/feed-admin/f95dfa31c793c857af6e7b65b5387a05f30d31ba.png",
-              "name" : "更多分区",
-              "uri" : "bilibili://main/top_category",
-              "pos" : 1
-            },
             {
               "id" : 922,
               "icon" : "http://i0.hdslb.com/bfs/feed-admin/38beac42189ad4d838d20259a5b2cdfd302fef40.png",
@@ -121,84 +61,13 @@ if (url.indexOf(feed_path) != -1) {
           },
           "bottom" : [
             {
-              "uri" : "bilibili://main/home/",
-              "tab_id" : "home",
-              "pos" : 1,
-              "id" : 177,
-              "icon_selected" : "http://i0.hdslb.com/bfs/archive/e5106aa688dc729e7f0eafcbb80317feb54a43bd.png",
-              "icon" : "http://i0.hdslb.com/bfs/archive/63d7ee88d471786c1af45af86e8cb7f607edf91b.png",
-              "name" : "首页"
-            },
-            {
-              "uri" : "bilibili://following/home/",
-              "tab_id" : "dynamic",
-              "pos" : 2,
-              "id" : 179,
-              "icon_selected" : "http://i0.hdslb.com/bfs/archive/25b658e1f6b6da57eecba328556101dbdcb4b53f.png",
-              "icon" : "http://i0.hdslb.com/bfs/archive/86dfbe5fa32f11a8588b9ae0fccb77d3c27cedf6.png",
-              "name" : "动态"
-            },
-            {
-              "uri" : "bilibili://uper/center_plus?relation_from=center_plus&tab_index=2",
-              "tab_id" : "publish",
-              "pos" : 3,
-              "id" : 670,
-              "dialog_items" : [
-                {
-                  "id" : 617,
-                  "icon" : "http://i0.hdslb.com/bfs/feed-admin/01f9b3f8ed61a4e59af693da9fcd38fc342ee7e5.png",
-                  "name" : "开直播",
-                  "uri" : "activity://liveStreaming/home?source_event=14"
-                },
-                {
-                  "id" : 618,
-                  "icon" : "http://i0.hdslb.com/bfs/feed-admin/30636aa60e594550ec47422e3875b4345e7d6017.png",
-                  "name" : "拍摄",
-                  "uri" : "bilibili://uper/user_center/add_archive/?from=1&is_new_ui=1&relation_from=center_plus"
-                },
-                {
-                  "id" : 619,
-                  "icon" : "http://i0.hdslb.com/bfs/feed-admin/55c3c112f4885adc6cce0b4b94149409fd1c147b.png",
-                  "name" : "上传",
-                  "uri" : "bilibili://uper/user_center/add_archive/?from=0&is_new_ui=1&relation_from=center_plus"
-                },
-                {
-                  "id" : 620,
-                  "icon" : "http://i0.hdslb.com/bfs/feed-admin/4e5188d8390754655dee0fdfd90c1088da3cdf90.png",
-                  "name" : "模板创作",
-                  "uri" : "bilibili://uper/user_center/add_archive/?from=2&is_new_ui=1&relation_from=center_plus"
-                }
-              ],
-              "publish_bubble" : [
-                {
-                  "id" : 20093,
-                  "icon" : "https://i0.hdslb.com/bfs/activity-plat/static/20221123/e736cc83e950d8b595cf52014b8f0242/0kqBwun7Wi.png",
-                  "stime" : 1673458265,
-                  "url" : "bilibili://uper/user_center/add_archive/?from=2&rhythm_id_v2=2050539&is_detail=1&post_config={\"first_entrance\":\"素材页\"}",
-                  "etime" : 1673631065
-                }
-              ],
-              "type" : 3,
-              "icon" : "http://i0.hdslb.com/bfs/feed-admin/c25cabacb40e9df2ccf54c327350e1afc4ae2f8c.png",
-              "name" : "发布"
-            },
-            {
-              "uri" : "bilibili://mall/home",
-              "tab_id" : "会员购Bottom",
-              "pos" : 4,
-              "id" : 242,
-              "icon_selected" : "http://i0.hdslb.com/bfs/archive/eeaf83fb7157000776dd93f61702a049f56801d3.png",
-              "icon" : "http://i0.hdslb.com/bfs/archive/6090d5fa7ece2a94de839e7cce4f1e774dae7779.png",
-              "name" : "会员购"
-            },
-            {
               "uri" : "bilibili://user_center/",
               "tab_id" : "我的Bottom",
               "pos" : 5,
               "id" : 181,
               "icon_selected" : "http://i0.hdslb.com/bfs/archive/a54a8009116cb896e64ef14dcf50e5cade401e00.png",
               "icon" : "http://i0.hdslb.com/bfs/archive/4b0b2c49ffeb4f0c2e6a4cceebeef0aab1c53fe1.png",
-              "name" : "大李昊同学"
+              "name" : "我的"
             }
           ]
         },

2912dff47f4ec6fb7ae27b6573ee9b6ab22412e0
HowieLi <chinahowieli@gmail.com>
2023-01-14 00:57:21 +0800

    update.

--- bilibili.js
+++ bilibili.js
@@ -2,10 +2,11 @@
 
 // 推荐视频
 const feed_path = "/x/v2/feed/index"
-// Tab资源按钮
-const resource_path = "/x/resource/show/tab/v2"
 // 热搜
 const hot_path = "/x/v2/search/square"
+const resource_path = "/x/resource/show/tab/v2"
+const resource_top_path = "/x/resource/top/activity"
+
 // 开屏广告
 const splash_path = "/x/v2/splash/show"
 
@@ -19,6 +20,11 @@ try {
 if (url.indexOf(feed_path) != -1) {
     console.log('feed...')
     body.data.items = []
+} else if (url.indexOf(hot_path) != -1) {
+    console.log('hot...')
+    // 删除热搜榜单
+    body.data[0].data = {}
+    body.data[0].search_ranking_meta.open_search_ranking = false
 } else if (url.indexOf(resource_path) != -1) {
     console.log('resource...')
     let tab = body.data.tab
@@ -39,11 +45,9 @@ if (url.indexOf(feed_path) != -1) {
     body.data.bottom = body.data.bottom.filter(function(item) {
         return item.uri === 'bilibili://user_center/'
     })
-} else if (url.indexOf(hot_path) != -1) {
-    console.log('hot...')
-    // 删除热搜榜单
-    body.data[0].data = {}
-    body.data[0].search_ranking_meta.open_search_ranking = false
+} else if (url.indexOf(resource_top_path) != -1) {
+    console.log("resource top...")
+    body.data.items = {}
 }
 
 let res = $response.body
